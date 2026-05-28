f443e5628470182f437a5ce410807a0d5729179d
Wanglida先生 <woaixiaomiguanguan@gmail.com>
2026-05-29 07:49:32 +0800

    Add app font tokens

---
+++ FitGlow/Core/Theme/AppFonts.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+enum AppFonts {
+    static let largeTitle = Font.system(size: 34, weight: .bold, design: .rounded)
+    static let heroNumber = Font.system(size: 64, weight: .bold, design: .rounded)
+    static let cardTitle = Font.system(size: 17, weight: .semibold)
+    static let body = Font.system(size: 15, weight: .medium)
+    static let caption = Font.system(size: 13, weight: .medium)
+}
